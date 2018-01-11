b5d2163655d9b489be655c0bc64f3e6c720cdf64
Yulia Drozdova <yuliannka@gmail.com>
2018-01-11 18:37:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function takeANumber(katzDeli, name) {
   katzDeli.push(`${name}`);
   return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`)
 }
+
+function nowServing() {
+  
+  
+}
