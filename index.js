b66adaf54a0ddc324e76a0bf33ded4c2c47c4d45
Yulia Drozdova <yuliannka@gmail.com>
2018-01-11 18:49:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function nowServing(deliLine) {
   if (deliLine.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-    return `Currently serving ${deliLine.shift()}`
+    return `Currently serving ${deliLine.shift()}.`
   }
 }
